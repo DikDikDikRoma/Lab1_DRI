6067f7acdcf26890b2571d593e36248aa2c51ad9
Romdik <ya-www-rom2013@ya.ru>
2023-09-16 00:54:19 +0700

    Добавлен метод GetSum

---
+++ Lab1_DRI/Lab1_DRI/Operation.cs
@@ -0,0 +1,46 @@
+﻿using Serilog;
+
+namespace Lab1_DRI
+{
+    internal class Operation
+    {
+        public static (string, int) GetSum(string a , string b) 
+        {
+            Log.Information($"Запрос суммы двух чисел: a={a}, b={b}");
+
+            try
+            {
+                int x = Convert.ToInt32(a);
+                int y = Convert.ToInt32(b);
+
+                if (ArePositive(x, y))
+                {
+                    string textResult = "OK";
+                    int sumResult = x + y;
+
+                    Log.Information($"Сумма чисел {a} и {b} равна {sumResult} и это {textResult}");
+
+                    return (textResult, sumResult);
+                }
+                else
+                {
+                    Log.Warning($"Сумма двух отрицательных чисел: {x} и {y}. Это NOK.");
+
+                    return ("NOK", -1);
+                }
+
+            }
+            catch (Exception exp)
+            {
+                Log.Fatal($"В качестве параметров переданы не числа: '{a}' и '{b}'. Это ERROR");
+                Log.Fatal($"{exp.Message}/n{exp.StackTrace}");
+                return ("ERROR", -2);
+            }
+        }
+
+        private static bool ArePositive(params int[] mass)
+        {
+            return mass.All (x => x > 0);
+        }
+    }
+}
--- Lab1_DRI/Lab1_DRI/Program.cs
+++ Lab1_DRI/Lab1_DRI/Program.cs
@@ -16,7 +16,12 @@ namespace Lab1_DRI
             Log.Debug("Application start");
 
             // Run
-
+            Operation.GetSum("1", "2");
+            Operation.GetSum("1", "2,0");
+            Operation.GetSum("1", "2.0");
+            Operation.GetSum("-1", "2");
+            Operation.GetSum("1", "-2");
+            Operation.GetSum("1s", "2d");
 
             Log.Debug("Application stop");
             Log.CloseAndFlush();
